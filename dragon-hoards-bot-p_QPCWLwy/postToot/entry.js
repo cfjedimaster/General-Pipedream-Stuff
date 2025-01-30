22750c68fe7d2fff74b51639b4267671b49cb87d
raymondcamden <raymondcamden@gmail.com>
2025-01-30 21:48:13 +0000

    commit before merging to production

--- dragon-hoards-bot-p_QPCWLwy/postToot/entry.js
+++ dragon-hoards-bot-p_QPCWLwy/postToot/entry.js
@@ -1,4 +1,5 @@
-import Mastodon from 'mastodon-api'
+import Mastodon from 'mastodon-api';
+import fs from 'fs';
 
 export default defineComponent({
   async run({ steps, $ }) {
@@ -8,9 +9,17 @@ export default defineComponent({
       api_url: 'https://mastodon.social/api/v1/', 
     });
 
+    let media_ids = [];
 
+    // handle no image result
+    if(steps.generateImage.$return_value) {
+      let resp = await M.post('media', { file: fs.createReadStream(steps.generateImage.$return_value) });
+      media_ids.push(resp.data.id)
+    }
+    
     await M.post('statuses', { 
         status: steps.generateStory.$return_value,
+        media_ids
     });
 
     return true;
